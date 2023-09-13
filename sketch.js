b2255a1e585a2059fa44f011a75f59c65b5ff1b2
Craig Moore <cdm21d@fsu.edu>
2023-09-13 11:29:31 -0400

    finished in-class demo, made ball 'bounce'

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
+let x = 200;
+let speed = 4;
+let diameter = 40;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(125);
+  ellipse(x, height/2, 40);
+  if (x >= width) {
+    speed = speed * -1;
+  }
+  if (x <= 0) {
+    speed = speed * -1;
+  }
+  x = x + speed;
 }
+
+// function dance() {
+//   let animation = frameCount;
+//   rect(animation, 200, 10, 10);
+//   console.log("We Dance");
+//   if (frameCount > 200) {
+//     let animation = frameCount *-1 ;
+//     console.log("We Animate");
+//   }
+// }
