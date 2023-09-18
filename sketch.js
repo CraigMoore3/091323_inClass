5f39ea9c91ceab01b328d07fcec2103c78e7e59d
Craig Moore <cdm21d@fsu.edu>
2023-09-18 11:14:08 -0400

    added rotation

--- sketch.js
+++ sketch.js
@@ -1,29 +1,21 @@
-let x = 200;
-let speed = 4;
-let diameter = 40;
-
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(125);
-  ellipse(x, height/2, 40);
-  if (x >= width || x<=0) {
-    speed = speed * -1;
-  }
-  // if (x <= 0) {
-  //   speed = speed * -1;
-  // }
-  x = x + speed;
-}
 
-// function dance() {
-//   let animation = frameCount;
-//   rect(animation, 200, 10, 10);
-//   console.log("We Dance");
-//   if (frameCount > 200) {
-//     let animation = frameCount *-1 ;
-//     console.log("We Animate");
-//   }
-// }
+  push();
+    translate(40, 40);
+    rotate(45);
+    rect(40, 40, 20, 20);
+  pop();
+
+  push();
+    translate(200, 80);
+    rotate(60);
+    rect(200, 80, 30, 20);
+  pop();
+}
